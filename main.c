093693c49bd905a7f3845ea748d64f74cbf249bc
Shrilalitha M <shrilalitham2004@gmail.com>
2025-03-08 10:15:29 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,9 +1,13 @@
-//
-//#include<stdio.h>
-//int main()
-//{
-//
-//    int number=10;
-//    printf("%c",&number);
-//
+//#include <stdio.h>
+//int main(){
+//FILE *file =fopen("example.txt", "w");
+//if(file==NULL){
+//    printf("Error opening file for writing.\n");
+//    return 1;
+//}
+//fprintf(file, "hello, world!\n");
+//fprintf(file, "this is a simple file handling example in C.\n");
+//fclose(file);
+//printf("data written to file successfully.\n");
+//return 0;
 //}
---
+++ main2.c
@@ -0,0 +1,23 @@
+//#include <stdio.h>
+//#include <stdlib.h>
+//
+//int main()
+//{
+//char item[50];
+//int count;
+//    FILE *file = fopen("example.txt","w");
+//    if(file==NULL)
+//    {
+//     printf("error opening the file for writing.\n");
+//    return 1;
+//}
+//for(count=0;count<=10;count++)
+//{
+//printf("enter a items:");
+//scanf("%s",&item);
+//fprintf(file,"%d:%s \n",count,&item);
+//}
+//fclose(file);
+//printf("data written to file successfully.\n");
+//return 0;
+//}
